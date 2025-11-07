ab5a83ec3b62ce46fb1991060058908ef3c200e9
afkarxyz <muhammadzamzamafkar@gmail.com>
2025-11-07 07:20:42 +0700

    .

---
+++ twitter-x-media-batch-downloader-pro.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name            Twitter/X Media Batch Downloader Pro
+// @name:zh         Twitter 媒体批量下载器 Pro
+// @name:ja         Twitter メディア一括ダウンローダー Pro
+// @description     Batch download all images & videos from any X/Twitter account — including withheld — in original quality, one click, no login.
+// @description:zh  批量下载任何 X/Twitter 账号的所有图片和视频（包括受限账号），原始质量，一键操作，无需登录。
+// @description:ja  X/Twitter アカウントからすべての画像と動画（制限付きアカウントを含む）を元の品質で一括ダウンロード、ワンクリック、ログイン不要。
+// @namespace       https://exyezed.cc
+// @supportURL      https://www.patreon.com/exyezed
+// @homepageURL     https://www.patreon.com/exyezed
+// @antifeature     payment Using this script requires an active monthly subscription.
+// @version         1.0.9
+// @author          exyezed
+// @license         MIT
+// @updateURL       https://github.com/exyezed/chatgpt-tts/releases/latest/download/twitter-x-media-batch-downloader-pro.meta.js
+// @downloadURL     https://github.com/exyezed/chatgpt-tts/releases/latest/download/twitter-x-media-batch-downloader-pro.user.js
+// ==/UserScript==
